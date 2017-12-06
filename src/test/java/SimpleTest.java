686002aa4f0f62f790ebb2b638eaf0d7ba2a95a5
Radomir Krawczykiewicz <kotwic4@gmail.com>
2017-12-06 12:28:57 +0100

    test integration

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleTest {
+
+    @Test
+    public void SimpleTest() {
+        assertEquals(3, 3);
+    }
+}
