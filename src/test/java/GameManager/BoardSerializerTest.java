03d8f0e9b3e7684040e472bad63ec354aba8eaa1
Michał Pawłowicz <michal.pawlowicz@yahoo.com>
2018-01-06 21:42:37 +0100

    test and dependencies

--- src/test/java/GameManager/BoardSerializerTest.java
+++ src/test/java/GameManager/BoardSerializerTest.java
@@ -12,10 +12,10 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 
 /**
- * Created by michal on 06.01.18.
+ * Created by Michał Pawłowicz on 06.01.18.
  */
 public class BoardSerializerTest {
-    private static String[] board1 = {
+    private final static String[] board1 = {
             "width=5",
             "height=5",
             "[1,1] [2,0] [3,0] [4,0] [5,0]",
@@ -26,8 +26,22 @@ public class BoardSerializerTest {
             null
     };
 
-    private static Board boardExpected;
-    static {
+    @Test
+    public void loadTest(){
+        BufferedReader br;
+        br = mock(BufferedReader.class);
+        try {
+            Mockito.when(br.readLine()).thenReturn(
+                    board1[0], board1[1],
+                    board1[2], board1[3],
+                    board1[4], board1[5],
+                    board1[6], board1[7]
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Board boardExpected;
         boardExpected = new Board(5, 5);
         Tile[][] tiles = boardExpected.getTiles();
         int tmp = 1;
@@ -37,11 +51,22 @@ public class BoardSerializerTest {
             }
         }
         tiles[0][0].setStronghold();
+
+        Board boardResult = null;
+        try {
+            boardResult = BoardSerializer.load(br);
+        } catch (BoardSerializer.MalformedFileException e) {
+            e.printStackTrace();
+        }
+        assertEquals(boardExpected.equals(boardResult), true);
     }
 
     @Test
-    public void loadTest(){
-        BufferedReader br = mock(BufferedReader.class);
+    public void loadTestIncorrect() {
+        Board boardExpected = null;
+        BufferedReader br = null;
+
+        br = mock(BufferedReader.class);
         try {
             Mockito.when(br.readLine()).thenReturn(
                     board1[0], board1[1],
@@ -49,17 +74,25 @@ public class BoardSerializerTest {
                     board1[4], board1[5],
                     board1[6], board1[7]
             );
-            Board boardResult = null;
-            try {
-                boardResult = BoardSerializer.load(br);
-            } catch (BoardSerializer.MalformedFileException e) {
-                e.printStackTrace();
-            }
-            assertEquals(boardExpected.equals(boardResult), true);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        boardExpected = new Board(5, 5);
+        Tile[][] tiles = boardExpected.getTiles();
+        int tmp = 1;
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                tiles[i][j].setCost(tmp++);
+            }
+        }
+
+        Board boardResult = null;
+        try {
+            boardResult = BoardSerializer.load(br);
+        } catch (BoardSerializer.MalformedFileException e) {
+            e.printStackTrace();
+        }
+        assertEquals(boardExpected.equals(boardResult), false);
     }
-
-
 }
